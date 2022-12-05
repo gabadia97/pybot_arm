10b854993a88d02023dcfa25c7b38a15bdd24a37
doyled529 <117535364+doyled529@users.noreply.github.com>
2022-12-05 17:42:35 -0500

    Add files via upload

--- pybot_arm/pybot_plot.py
+++ pybot_arm/pybot_plot.py
@@ -5,7 +5,6 @@ import matplotlib.animation as animation
 import pandas as pd
 
 
-
 ##############################################################################
 
 def read_csv_data(csv_file):
@@ -72,10 +71,10 @@ def create_plots( data, time, plot_type):
     
     if plot_type == 'control':
         fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(8, 8))
-        fig.tight_layout(pad=3)
-        ax1.plot(data[0], time)
-        ax2.plot(data[1], time)
-        ax3.plot(data[2],time)
+        fig.tight_layout(pad=4)
+        ax1.plot(time, data[0])
+        ax2.plot(time, data[1])
+        ax3.plot(time, data[2])
         fig.suptitle('Control vs Time')
         ax1.set_title('rho-control vs time')
         ax2.set_title('theta-control vs time')
@@ -88,30 +87,30 @@ def create_plots( data, time, plot_type):
         ax.set_ylabel('y')
         ax.set_zlabel('z')
         plt.title('Position vs Time')
-        plt.plot(position[0], position[1], position[2])
+        plt.plot(data[0], data[1], data[2])
         
         fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(8, 8))
-        fig.tight_layout(pad=3)
-        ax1.plot(data[0], time)
-        ax2.plot(data[1], time)
-        ax3.plot(data[2],time)
+        fig.tight_layout(pad=4)
+        ax1.plot(time, data[0])
+        ax2.plot(time, data[1])
+        ax3.plot(time, data[2])
         fig.suptitle('Position vs Time')
-        ax1.set_title('x-position vs Time')
-        ax2.set_title('y-position vs Time')
-        ax3.set_title('z-position vs Time')
+        ax1.set_title('x-position vs time')
+        ax2.set_title('y-position vs time')
+        ax3.set_title('z-position vs time')
         
         
     
     elif plot_type == 'velocity' or plot_type == 'acceleration':
         fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(8, 8))
-        fig.tight_layout(pad=3)
-        ax1.plot(data[0], time)
-        ax2.plot(data[1], time)
-        ax3.plot(data[2],time)
+        fig.tight_layout(pad=4)
+        ax1.plot(time, data[0])
+        ax2.plot(time, data[1])
+        ax3.plot(time, data[2])
         fig.suptitle(f'{plot_type.capitalize()} vs Time')
-        ax1.set_title(f'x-{plot_type} vs Time')
-        ax2.set_title(f'y-{plot_type} vs Time')
-        ax3.set_title(f'z-{plot_type} vs Time')
+        ax1.set_title(f'x-{plot_type} vs time')
+        ax2.set_title(f'y-{plot_type} vs time')
+        ax3.set_title(f'z-{plot_type} vs time')
        
     else:
         raise ValueError('plot_type must be position, velocity, acceleration, or control')
@@ -124,24 +123,39 @@ def animate_3D(position):
     This function creates 3D animation given the x, y, and z data of the trajectory. 
     
     """
+
+    # def func(num, dataSet, line):
+    #     line.set_data(dataSet[0:2, :num])
+    #     line.set_3d_properties(dataSet[2, :num])
+        
+    # dataSet = np.array([position[0], position[1], position[2]])
+    # numDataPoints = len(position[0])
+    
+    # fig = plt.figure()
+    # ax = Axes3D(fig)
+    # line =plt.plot(dataSet[0], dataSet[1], dataSet[2], lw=2, c='g')[0]
+    # ax.set_xlabel('X(t)')
+    # ax.set_ylabel('Y(t)')
+    # ax.set_zlabel('Z(t)')
+    
+    # line_ani = animation.FuncAnimation(fig, func, frames=numDataPoints, fargs=(dataSet, line))
+    # plt.show()
     
     def func(num, dataSet, line):
         line.set_data(dataSet[0:2, :num])
         line.set_3d_properties(dataSet[2, :num])
-        
-    #dataSet = np.array([x, y, z])
-    #dataSet = position
+    
+    dataSet = np.array([position[0], position[1], position[2]])
     numDataPoints = len(position[0])
     
     fig = plt.figure()
     ax = Axes3D(fig)
-    #line =plt.plot(dataSet[0], dataSet[1], dataSet[2], lw=2, c='g')[0]
-    line = plt.plot(position[0], position[1], position[2], lw=2, c='g')[0]
+    line = plt.plot(dataSet[0], dataSet[1], dataSet[2], lw=2, c='g')[0]
     ax.set_xlabel('X(t)')
     ax.set_ylabel('Y(t)')
     ax.set_zlabel('Z(t)')
     
-    line_ani = animation.FuncAnimation(fig, func, frames=numDataPoints, fargs=(position, line))
-    plt.show()
+    animate = animation.FuncAnimation(fig, func, frames=numDataPoints, fargs=(dataSet, line))
+
 
 ##############################################################################
